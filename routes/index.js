c05997a037d0fb83476b68c4e796db98f7e99cdd
Pavan Choudhary <choudhary.pavan18@gmail.com>
2019-03-09 15:00:03 +0530

    sql added

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,45 @@
 var express = require('express');
 var router = express.Router();
 var mod=require('../models/schema.js');
+var mysql      = require('mysql');
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : 'root',
+   database : 'qms'
+ });
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  console.log("API request at .");
-  var RF_14={
-    title: "Test Format- RF-14",
-      description: "Determination of materials Finer than 75 μm sieve.",
-      type: "object",
-      properties:{
-        details:{
-          title:"Sample Details",
-          type: "object",
-          properties:{
-            Supplier:{type:"string" },
-            date:{type:"number" ,title:"Date of Test" },
-            Source:{type:"string" },
-            Plant:{type:"string" },
-            Aggregate_Type: {
-              "type": "array",
-              "title": "Aggregate Types",
-              "items": {
-                "type": "string",
-                "enum": [ "River Sand", "Crushed Sand","Crushed Rock Fines"]
-              },
-              "uniqueItems": true
-            },
+router.post('/', function(req, res) {
+  var dt =new Date();
+  connection.query('INSERT INTO form_details (user, submit_date) VALUES (?,?) ',["pavan",dt], function(err, rows, fields) {
+    if (!err){
+      var new_id;
+      var val=[];
+      connection.query("SELECT MAX(id) FROM form_details", function (err, result2, fields) {
+        if (err) throw err;
+        new_id=result2[0]['MAX(id)'];
+        for(key in req.body){
+          var x=[];
+          x.push(key,req.body[key],new_id);
+          val.push(x);
+        }
+        connection.query('INSERT INTO submissions (question,answer,formid) VALUES ? ',[val], function(err,result) {
+          if (!err){
+            console.log("data inserted");
+            res.send("success");
           }
-        },
-      }
-  };
-  res.send({"form":RF_14});
+          else
+            console.log('Error while performing Query2.'+err);
+        });
+      });
+    }
+    else
+      console.log('Error while performing Query.'+err);
+  });
+  //connection.end();
+  
+  res.send({"msg":"submitted"});
 });
 
 router.post('/submit', function(req, res) {
